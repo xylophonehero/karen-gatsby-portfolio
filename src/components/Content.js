bb996d5d81eed76c7e23ce377b93133ef83b1605
XylophoneHero <nick97000@gmail.com>
2021-08-01 09:55:31 +0100

    no broken html

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 function Content({ text, ...props })
 {
+  if (!text) return null
   return (
     <Box className="content" {...props} dangerouslySetInnerHTML={{ __html: text.childMarkdownRemark.html }} />
   );
